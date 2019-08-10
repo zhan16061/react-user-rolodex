68c781d596601ae9f9e9873348ee8c172edc45f3
國展 <zhan16061@gmail.com>
2019-08-10 16:43:58 +0800

    Updates

---
+++ precache-manifest.49f24243031cba646c02d45509d2b781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601e9eb4704c5fcd2f0f243689ec9b59",
+    "url": "/react-user-rolodex/index.html"
+  },
+  {
+    "revision": "f3dd254cfc2432724f43",
+    "url": "/react-user-rolodex/static/css/main.ded44b3b.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/react-user-rolodex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "f3dd254cfc2432724f43",
+    "url": "/react-user-rolodex/static/js/main.6b1d7190.chunk.js"
+  },
+  {
+    "revision": "ca471009f97144763ffa",
+    "url": "/react-user-rolodex/static/js/runtime~main.349e68f9.js"
+  }
+]);
